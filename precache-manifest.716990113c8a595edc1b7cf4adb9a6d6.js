6739d04e1c9f69508a26b952b9aa4d2763328437
= <immy01@hotmail.com>
2020-09-02 12:31:35 +0100

    Updates

---
+++ precache-manifest.716990113c8a595edc1b7cf4adb9a6d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2fb82b6770976de1598a98a517dae3",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "5abd3e3d4beda18806ba",
+    "url": "/react-snake-game/static/css/main.86053096.chunk.css"
+  },
+  {
+    "revision": "6253f1e68441509253dc",
+    "url": "/react-snake-game/static/js/2.570435a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-snake-game/static/js/2.570435a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abd3e3d4beda18806ba",
+    "url": "/react-snake-game/static/js/main.9331717c.chunk.js"
+  },
+  {
+    "revision": "bf6b57f96c8990e054fc",
+    "url": "/react-snake-game/static/js/runtime-main.03bd9a2c.js"
+  }
+]);
